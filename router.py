985c94ab0e707ed303d5f5406c6e62ff9b80da4f
lepus64 <lepus64@gmail.com>
2024-07-01 15:30:15 +0300

    first commit

---
+++ router.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter, Depends
+from typing import Annotated
+from repository import TaskRepository
+from schemas import STask, STaskAdd, STaskId
+
+router = APIRouter(
+    prefix = "/tasks",
+    tags = ["Таски"], 
+    
+)
+
+@router.post("")
+async def add_task(task: Annotated[STaskAdd, Depends()], ) -> STaskId:
+    task_id = await TaskRepository.add_one(task)
+    return {"ok": True, "task_id": task_id}
+
+@router.get("")
+async def get_tasks() -> list[STask]:
+    tasks = await TaskRepository.get_tasks()
+    return tasks
